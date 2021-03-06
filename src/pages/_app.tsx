1b62e9657e7c7f9ec2be26b3f611f3fc0413c496
Mateus Merscher <mateus.merscher@filmefilme.com.br>
2021-03-06 17:58:20 -0300

    theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 //import '../styles/global.css';
 
 import GlobalStyle from '../styles/global';
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+import light from '../styles/themes/light';
+import dark from '../styles/themes/dark';
 
 function MyApp({ Component, pageProps }) {
-
+  const [theme, setTheme] = useState(light);
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }
   return (
     <div>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+
+        <GlobalStyle />
+        <Component toggleTheme={toggleTheme} {...pageProps} />
+      </ThemeProvider>
+
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import styles from '../styles/pages/Home.module.css';
 import Head from 'next/head';
 import React from "react";
-import light from '../styles/themes/light';
-import dark from '../styles/themes/dark';
 import { GetServerSideProps } from 'next';
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -12,51 +10,45 @@ import { ChallengeBox } from "../components/ChallengeBox";
 import { ConfigButton } from '../components/ConfigButton';
 import { ChallengesProvider } from "../contexts/ChallengesContext";
 import { CountdownProvider } from "../contexts/CountdownContext";
-import { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
 
 interface HomeProps {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  toggleTheme: () => void;
 }
 
 export default function Home(props: HomeProps) {
-  const [theme, setTheme] = useState(light);
-  const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light);
-  }
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <ChallengesProvider
-          level={props.level}
-          currentExperience={props.currentExperience}
-          challengesCompleted={props.challengesCompleted}
-        >
-          <div className={styles.container}>
-            <Head>
-              <title>Início | Spostalo</title>
-            </Head>
-            <header className={styles.header}>
-              <ExperienceBar />
-              <ConfigButton toggleTheme={toggleTheme} />
-            </header>
-            <CountdownProvider>
-              <section>
-                <div>
-                  <Profile />
-                  <CompletedChallenges />
-                  <Countdown />
-                </div>
-                <div>
-                  <ChallengeBox />
-                </div>
-              </section>
-            </CountdownProvider>
-          </div>
-        </ChallengesProvider>
-      </ThemeProvider>
+      <ChallengesProvider
+        level={props.level}
+        currentExperience={props.currentExperience}
+        challengesCompleted={props.challengesCompleted}
+      >
+        <div className={styles.container}>
+          <Head>
+            <title>Início | Spostalo</title>
+          </Head>
+          <header className={styles.header}>
+            <ExperienceBar />
+            <ConfigButton toggleTheme={props.toggleTheme} />
+          </header>
+          <CountdownProvider>
+            <section>
+              <div>
+                <Profile />
+                <CompletedChallenges />
+                <Countdown />
+              </div>
+              <div>
+                <ChallengeBox />
+              </div>
+            </section>
+          </CountdownProvider>
+        </div>
+      </ChallengesProvider>
     </>
   )
 }
